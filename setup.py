5a3efcc555f0d019a166b6e82165785be6d0337f
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-27 12:38:18 +1000

    Updated to version 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 package_name = "synerty-peek"
-package_version = '0.0.0'
+package_version = '0.0.7'
 
 egg_info = "%s.egg-info" % package_name
 if os.path.isdir(egg_info):
