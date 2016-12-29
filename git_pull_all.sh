6410f508d968347e92621324c22bd3040e2c204d
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-30 08:33:02 +1000

    Updated git scripts, fixed  --commit in wrong script

--- git_pull_all.sh
+++ git_pull_all.sh
@@ -5,5 +5,5 @@ source ./pip_common.sh
 # -------------------------------------
 for pkgDir in `ls -d ../peek-*` ../synerty-peek; do
     echo "Pulling ${pkgDir}"
-    (cd $pkgDir && git pull)
+    (cd $pkgDir && git pull --commit)
 done
--- git_push_all.sh
+++ git_push_all.sh
@@ -5,5 +5,5 @@ source ./pip_common.sh
 # -------------------------------------
 for pkgDir in `ls -d ../peek-*` ../synerty-peek; do
     echo "Pushing ${pkgDir}"
-    (cd $pkgDir && git push --commit)
+    (cd $pkgDir && git push)
 done
