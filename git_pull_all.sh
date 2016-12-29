e018221ed868b3b3290e0f63de517c0b6b582e78
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-30 08:18:06 +1000

    Added script to git pull all

---
+++ git_pull_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source ./pip_common.sh
+
+# -------------------------------------
+for pkgDir in `ls -d ../peek-*`; do
+    echo "Pulling ${pkgDir}"
+    (cd $pkgDir && git pull)
+done
