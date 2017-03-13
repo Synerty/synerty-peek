96f78f2bf7e37e7527b863932abd653983026cbe
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-13 16:16:15 +1000

    Updated to version 0.0.28

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@
 __project__ = 'Synerty Peek'
 __copyright__ = '2016, Synerty'
 __author__ = 'Synerty'
-__version__ = '0.0.27'
+__version__ = '0.0.28'
 
 import sphinx_rtd_theme
 
@@ -64,7 +64,7 @@ author = __author__
 # built documents.
 #
 # The short X.Y version.
-__version__ = '0.0.27'
+__version__ = '0.0.28'
 # The full version, including alpha/beta/rc tags.
 release = __version__
 
