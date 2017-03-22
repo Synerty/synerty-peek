b8ba1fff1e65d550c698210d150d0beb2b1f4f56
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-23 07:25:40 +1000

    Updated to version 0.2.10

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@
 __project__ = 'Synerty Peek'
 __copyright__ = '2016, Synerty'
 __author__ = 'Synerty'
-__version__ = '0.2.9'
+__version__ = '0.2.10'
 
 import sphinx_rtd_theme
 
@@ -64,7 +64,7 @@ author = __author__
 # built documents.
 #
 # The short X.Y version.
-__version__ = '0.2.9'
+__version__ = '0.2.10'
 # The full version, including alpha/beta/rc tags.
 release = __version__
 
