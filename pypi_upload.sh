0d1129ade9cebee85307c7016ecd6df11da1bf17
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-04-12 16:54:48 +1000

    Added commented out twine line to upload

--- pypi_upload.sh
+++ pypi_upload.sh
@@ -9,6 +9,8 @@ echo
 echo "Uploading to PYPI"
 EXIT=""
 for pkg in $PACKAGES; do
+    # if ! (cd ../$pkg && rm -rf dist/* && python setup.py sdist && twine upload dist/*); then
+
     if ! (cd ../$pkg && python setup.py sdist upload -r pypi); then
         echo "${bold}${pkg}${normal} : Failed to build and upload to pypi" >&2
         exit 1
