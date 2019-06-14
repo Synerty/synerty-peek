424b5c09aee006673c84065c7e4b11eb7ba91a84
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-14 21:43:51 +1000

    Added script to push to github

---
+++ git_push_github.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source ./pip_common.sh
+
+# -------------------------------------
+for pkgDir in `cd ../ && ls -d peek-*` synerty-peek; do
+    if [[ ${pkgDir} == *"pof"* ]]; then continue; fi
+    if [[ ${pkgDir} == *"gis"* ]]; then continue; fi
+
+    echo "Pushing ${pkgDir}"
+    pushd ../$pkgDir
+    git remote remove github 2> /dev/null || true
+    git remote add github git@github.com:Synerty/${pkgDir}.git
+    git push -f github master || true
+    popd
+done
+
