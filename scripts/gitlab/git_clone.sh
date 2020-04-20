6f97c991e88b3312c8682143839e8e1dd38096db
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-04-20 20:22:41 +1000

    Pipelines can now build all in a branch, or all from users namespace
    PEEK-566

--- scripts/gitlab/git_clone.sh
+++ scripts/gitlab/git_clone.sh
@@ -14,17 +14,32 @@ fi
 SRC_PATH=${2:-/tmp}
 USER=${3}
 PASSWORD=${4}
+CI_COMMIT_REF_NAME=${5}
+CI_PROJECT_NAMESPACE=${6}
 
 echo "Cloning all the repositories at ${SRC_PATH}"
 
 mkdir -p  ${SRC_PATH} && cd ${SRC_PATH}
 
+# Clone the repos
 for repo in ${PACKAGES}; do
     rm -fR ${repo}
-    echo ${repo}
-    git clone https://${USER}:${PASSWORD}@gitlab.synerty.com/peek/${repo}.git &
+    # Shallow? --depth 5
+    git clone https://${USER}:${PASSWORD}@gitlab.synerty.com/${CI_PROJECT_NAMESPACE}/${repo}.git &
 done
 
 # Wait for background jobs to finish
 wait
+
+# Try to switch to a specific branch, otherwise use master
+for repo in ${PACKAGES}; do
+    pushd ${repo}
+    if git fetch origin "${CI_COMMIT_REF_NAME}"; then
+        echo "${repo}: Checking out ${CI_COMMIT_REF_NAME}"
+        git checkout "${CI_COMMIT_REF_NAME}"
+    else
+        echo "${repo}: Failed to switch to ${CI_COMMIT_REF_NAME} using master instead"
+    fi
+    popd
+done
 
