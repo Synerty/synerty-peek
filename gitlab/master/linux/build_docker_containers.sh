1ea8f7e1d937c537140aa7951b149c9434fc1004
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-06-25 11:29:14 +1000

    PEEK-644: imp: master dockers tagged as master AND latest

--- gitlab/master/linux/build_docker_containers.sh
+++ gitlab/master/linux/build_docker_containers.sh
@@ -3,15 +3,22 @@
 set errexit
 set nounset
 
-BRANCH=master
+function build_containers() {
+    BRANCH=$1
 
-for IMAGE_NAME in peek-linux peek-linux-build peek-linux-doc peek-linux-test
-do
-    IMAGE_NAME="${IMAGE_NAME}:${BRANCH}"
-    echo "Building |${IMAGE_NAME}|"
+    for IMAGE_NAME in peek-linux peek-linux-build peek-linux-doc peek-linux-test
+    do
+        IMAGE_NAME="${IMAGE_NAME}:${BRANCH}"
+        echo "Building |${IMAGE_NAME}|"
 
-    docker build --no-cache -t ${IMAGE_NAME} -f ${IMAGE_NAME}.Dockerfile .
-    docker tag ${IMAGE_NAME} nexus.synerty.com:5000/${IMAGE_NAME}
-    docker push nexus.synerty.com:5000/${IMAGE_NAME}
+        docker build --no-cache -t ${IMAGE_NAME} -f ${IMAGE_NAME}.Dockerfile .
+        docker tag ${IMAGE_NAME} nexus.synerty.com:5000/${IMAGE_NAME}
+        docker push nexus.synerty.com:5000/${IMAGE_NAME}
 
-done
+    done
+
+}
+
+# No tag == "latest"
+build_containers latest
+build_containers master
