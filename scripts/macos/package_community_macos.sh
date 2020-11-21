67cafbe67d3049a08db6512e61195df98e170e35
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-11-21 04:45:01 +0000

    PEEK-562, Create macOS gitlab builds

--- scripts/macos/package_community_macos.sh
+++ scripts/macos/package_community_macos.sh
@@ -12,9 +12,12 @@ if [ -n ${wantedVer} ]; then
     echo "Requested version is $wantedVer"
 fi
 
-startDir=`pwd`
+platformReposDir=${2:-nodir}
+platformPackagesDir=${3:-nodir}
+startDir=${4:-`pwd`}
+pinnedDepsPyFile=${5:-nofile}
 
-baseDir="$startDir/peek_platform_macos"
+baseDir="$startDir/peek_community_macos"
 
 [ -d $baseDir ] && rm -rf $baseDir
 
@@ -26,15 +29,23 @@ baseDir="$startDir/peek_platform_macos"
 mkdir -p $baseDir/py
 cd $baseDir/py
 
+pipWheelArgs=" --no-cache --find-links=${platformPackagesDir}"
+if [ -f "${pinnedDepsPyFile}" ]; then
+    echo "Using requirements file : ${pinnedDepsPyFile}"
+    pipWheelArgs="-r ${pinnedDepsPyFile} $pipWheelArgs"
+else
+    echo "Requirements file is missing : ${pinnedDepsPyFile}"
+fi
+
 echo "Downloading and creating wheels"
 if [ -n ${wantedVer} ]; then
-    pip wheel --no-cache synerty-peek==${wantedVer}
+    pip wheel synerty-peek==${wantedVer} $pipWheelArgs
 else
-    pip wheel --no-cache synerty-peek
+    pip wheel synerty-peek $pipWheelArgs
 fi
 
 # Make sure we've downloaded the right version
-peekPkgVer=`ls synerty_peek-* | cut -d'-' -f2`
+peekPkgVer=`cd $baseDir/py && ls synerty_peek-* | cut -d'-' -f2`
 
 if [ -n "${wantedVer}" -a "${wantedVer}" != "${peekPkgVer}" ]; then
    echo "We've downloaded version ${peekPkgVer}, but you wanted ver ${wantedVer}"
@@ -81,18 +92,29 @@ npm -g install @angular/cli@^9.1.2 typescript@3.8.3 tslint
 function downloadNodeModules {
     # Get the variables for this package
     nmDir=$1
+
     packageJsonUrl="$2/package.json"
     packageLockJsonUrl="$2/package-lock.json"
+
+    packageJsonDir="$3/package.json"
+    packageLockJsonDir="$3/package-lock.json"
 
     # Create the tmp dir
     mkdir -p "$nmDir/tmp"
     cd "$nmDir/tmp"
 
-    # Download package.json
-    curl -O "$packageJsonUrl"
+    if [ -f ${packageJsonDir} ]
+    then
+        # Download package.json
+        cp "$packageJsonDir" .
 
-    # Download package-lock.json
-    curl -O "$packageLockJsonUrl"
+        cp "$packageLockJsonDir" .
+    else
+        # Download package.json
+        curl -O "$packageJsonUrl" .
+
+        curl -O "$packageLockJsonUrl" .
+    fi
 
     # run npm install
     npm install
@@ -106,23 +128,49 @@ function downloadNodeModules {
     rm -rf tmp
 }
 
-# MOBILE node modules
+# FIELD node modules
 mobilePackageVer=`cd $baseDir/py && ls peek_field_app-* | cut -d'-' -f2`
-mobileBuildWebDIR="$baseDir/mobile-build-web"
+mobileBuildWebDIR="$baseDir/field-app"
 mobileJsonUrl="https://bitbucket.org/synerty/peek-field-app/raw/${mobilePackageVer}/peek_field_app"
-downloadNodeModules $mobileBuildWebDIR $mobileJsonUrl
+mobileJsonDir="${platformReposDir}/peek-field-app/peek_field_app"
+downloadNodeModules $mobileBuildWebDIR $mobileJsonUrl $mobileJsonDir
 
-# DESKTOP node modules
+# OFFICE node modules
 desktopPackageVer=`cd $baseDir/py && ls peek_office_app-* | cut -d'-' -f2`
-desktopBuildWebDIR="$baseDir/desktop-build-web"
+desktopBuildWebDIR="$baseDir/office-app"
 desktopJsonUrl="https://bitbucket.org/synerty/peek-office-app/raw/${desktopPackageVer}/peek_office_app"
-downloadNodeModules $desktopBuildWebDIR $desktopJsonUrl
+desktopJsonDir="${platformReposDir}/peek-office-app/peek_office_app"
+downloadNodeModules $desktopBuildWebDIR $desktopJsonUrl $desktopJsonDir
 
 # ADMIN node modules
 adminPackageVer=`cd $baseDir/py && ls peek_admin_app-* | cut -d'-' -f2`
-adminBuildWebDIR="$baseDir/admin-build-web"
+adminBuildWebDIR="$baseDir/admin-app"
 adminJsonUrl="https://bitbucket.org/synerty/peek-admin-app/raw/${adminPackageVer}/peek_admin_app"
-downloadNodeModules $adminBuildWebDIR $adminJsonUrl
+adminJsonDir="${platformReposDir}/peek-admin-app/peek_admin_app"
+downloadNodeModules $adminBuildWebDIR $adminJsonUrl $adminJsonDir
+
+# ------------------------------------------------------------------------------
+# Copy over the init scripts for this platform
+
+# mkdir $baseDir/init && pushd $baseDir/init
+
+# for s in peek_logic_service peek_worker_service peek_agent_service peek_office_service peek_field_service
+# do
+#     cp ${platformReposDir}/synerty-peek/scripts/macos/init/${s}.service ${s}.service
+# done
+# popd
+
+# ------------------------------------------------------------------------------
+# Copy over the util scripts for this platform
+
+# mkdir $baseDir/util && pushd $baseDir/util
+
+# utilScripts="restart_peek.sh stop_peek.sh"
+# for s in $utilScripts
+# do
+#     cp ${platformReposDir}/synerty-peek/scripts/linux/util/${s} ${s}
+# done
+# popd
 
 # ------------------------------------------------------------------------------
 # Set the location back to where we were.
