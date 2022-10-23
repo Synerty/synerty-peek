6379fec19202adf646cf56e0e8c2c87a6656e8d2
GitLab CI <gitlab-ci@synerty.com>
2022-10-23 07:46:14 +0000

    Updated conf.py to 3.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ from sphinx.ext.apidoc import create_modules_toc_file, recurse_tree
 __project__ = "SynertyPeek"
 __copyright__ = "2016, Synerty"
 __author__ = "Synerty"
-__version__ = '3.3.1'
+__version__ = '3.3.2'
 
 import sphinx_rtd_theme
 
