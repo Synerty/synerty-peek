0fc09f9e3da8b972065d258ae8960737fc73c835
Louis Lu <louis.lu@synerty.com>
2023-08-24 16:50:45 +1200

    PEEK-2171 updated version tagging v2

--- publish.sh
+++ publish.sh
@@ -40,7 +40,7 @@ HAS_GIT=$(ls -d .git 2>/dev/null)
 # Set the versions
 echo "Setting version to $VER"
 
-VER_FILES="${VER_FILES} pyproject.toml"
+VER_FILES="${VER_FILES}"
 VER_FILES="${VER_FILES} ${PY_PACKAGE}/__init__.py"
 VER_FILES="${VER_FILES} ${PY_PACKAGE}/plugin_package.json"
 
@@ -50,9 +50,22 @@ function updateFileVers() {
         if [ -f ${file} ]
         then
             sed -i "s/^__version__.*/__version__ = \'${VER}\'/g" ${file}
-            sed -i 's/version = "0.0.0"/version = "'${VER}'"/g' ${file}
+            sed -i "s/0.0.0/${VER}/g" ${file}
         fi
     done
+
+    # update version in pyproject.toml
+    sed -i 's/version = "0.0.0"/version = "'${VER}'"/g' pyproject.toml
+    # update peek dependencies version in pyproject.toml
+    # e.g.
+    # replace "peek-admin-doc==0.0.*,>=0.0.0" with
+    #   "peek-admin-doc==4.1.*,>=4.1.2+b12345"
+    #   given version 4.1.0+b12345
+    _versionArray=( ${VER//./ } )
+    VERSION_MAJOR="${_versionArray[0]}"
+    VERSION_MINOR="${_versionArray[1]}"
+    sed 's/\(peek-.*==\).*\(,>=\)0.0.0/\1'"${VERSION_MAJOR}"'.'"${VERSION_MINOR}"'.\*\2'"${VER}"'/g' pyproject.toml
+
 }
 
 # Apply the version to the other files
